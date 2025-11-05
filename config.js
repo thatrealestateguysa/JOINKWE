239ac5a36c72ccf3aeb73107412f77732bff9389
thatrealestateguysa <dawie.dutoit@kwsa.co.za>
2025-11-05 06:48:23 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.KWX_CONFIG = {
-  API_ENDPOINT: 'https://script.google.com/macros/s/AKfycbzASeIT0m-L04odltNf4kepiO8MLqbaYP9BR9H89WCkxaCNHQ_p0U10roxmiBf6Nm2i0w/exec'
+  API_ENDPOINT: 'https://script.google.com/macros/s/AKfycbywav5WGn63g8HMZMGwzbluiZDbZu8ytFvFHGAd_gTT97RDK3-b9QGF3t-zVmiW9d1E/exec'
 };
